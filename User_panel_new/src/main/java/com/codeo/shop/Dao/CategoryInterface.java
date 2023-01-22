82644fce9fd36d1f0f701282a5d3bbb79d85cdac
payaltitirmare <payaltitirmare18@gmail.com>
2023-01-22 18:41:04 +0530

    User Panel

---
+++ User_panel_new/src/main/java/com/codeo/shop/Dao/CategoryInterface.java
@@ -0,0 +1,12 @@
+package com.codeo.shop.Dao;
+
+import java.util.List;
+
+import com.codeo.shop.entity.Category;
+
+public interface CategoryInterface {
+
+
+	public List<Category> getCategoryList();
+	
+}
